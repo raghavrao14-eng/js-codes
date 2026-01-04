d45e0e7de3b8a18e93ff095a8020deeccf8e447d
raghavrao14-eng <raghavrao306@gmail.com>
2026-01-04 04:28:06 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as modern
+// alert(3+3) // we are using nodejs, not browser so alert will not work
+let name = "Raghav";
+let age = 22;
+// number =>2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique 
+
+
+console.log(typeof "Raghav");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
